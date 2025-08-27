eb1d0462433b29e094d666df0cf2fd71d8283db3
Artur F <carpetblv@hotmail.com>
2025-08-27 00:00:59 -0300

    Atualização - Credenciais/Assinatura digital

--- Teste-01.py
+++ Teste-01.py
@@ -4,3 +4,17 @@ import hashlib
 import requests
 import pyautogui
 
+#credenciais api
+
+partner_id = 'sua id de afiliado'
+partner_key = 'sua chave de acesso'
+shop_id = 'id da loja'
+
+#Assinatura
+
+def sign (path, timestamp):
+    raw = f'{partner_id}{path}{timestamp}{shop_id}'
+    return hmac.new (partner_key.encode(), raw.encode(), hashlib.sha256).hexdigest()
+
+
+
