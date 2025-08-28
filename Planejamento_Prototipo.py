1db09df6713c4b8a9bc40dd040e00afc99418744
Artur F <carpetblv@hotmail.com>
2025-08-28 00:44:53 -0300

    Inicio da fase de prototipo - Planejamento

---
+++ Planejamento_Prototipo.py
@@ -0,0 +1,22 @@
+import time
+import pyautogui
+
+time.sleep(5)
+pyautogui.PAUSE = 1
+print (pyautogui.position())
+
+# Browser - Point(x=881, y=748)
+# Nova janela - Point(x=833, y=556)
+# Write ('link')
+# Press enter
+# Double click - Point(x=1156, y=29)
+# Botão direito - Point(x=337, y=405)
+# Aperta seta para cima 7 vezes
+# Aperta enter
+# Write ('1')
+# Aperta enter
+# Fechar a pagina - (x=1345, y=20)
+
+#----------------------#
+# Aqui entra o scrip de postagem no instagram
+#----------------------#
